70a0c6e109e27c164d41ed2195911f1ca48596f1
ycl818 <kevinyichienlee@gmail.com>
2022-01-18 16:42:29 -0500

    ADD: Rows style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import './App.css';
-import Row from './Row'
-import requests from './request';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hey dddd</h1>
-     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <h1>Hey dddd</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
