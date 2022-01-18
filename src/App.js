facf4d68fe77be4066c906feaf6f3c9b4536b1b2
ycl818 <kevinyichienlee@gmail.com>
2022-01-18 16:11:45 -0500

    Already fetch data

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Row from './Row'
+import requests from './request';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hey dddd</h1>
+     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Row.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  // a shippet of code which run based on a specific condition/variable
+  useEffect(() => {
+    // if [], run once when the row loads, and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      // "https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213"
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/* container -> post */}
+    </div>
+  );
+}
+
+export default Row;
