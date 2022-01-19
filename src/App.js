fb43fab5f45b81b2d33dac6f1321e28903854088
ycl818 <kevinyichienlee@gmail.com>
2022-01-18 21:53:54 -0500

    ADD: banner component and its styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Row from "./Row";
-import requests from "./request";
+import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey dddd</h1>
+      {/* <Nav /> */}
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
