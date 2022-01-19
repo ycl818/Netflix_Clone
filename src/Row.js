ce68bb293de2380dc42533eaf7b923f2a04ad7ec
ycl818 <kevinyichienlee@gmail.com>
2022-01-19 00:31:47 -0500

    ADD: youtube movie-trailer

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // a shippet of code which run based on a specific condition/variable
   useEffect(() => {
@@ -21,6 +24,52 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
   console.log(movies);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    // if opened close it
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else if (movie.id == "93405") {
+      //squid game
+      return setTrailerUrl("sH4Y450PSVM");
+    } else if (movie.id == "77169") {
+      return setTrailerUrl("LcDQqGJG8pA"); //cobra kai
+    } else if (movie.id == "96777") {
+      return setTrailerUrl("Af_Hj0MDBBQ"); //the silent sea
+    } else if (movie.id == "134949") {
+      return setTrailerUrl("BJYJksHREIc"); //Rebelde
+    } else if (movie.id == "94605") {
+      return setTrailerUrl("4Ps6nV4wiCE"); //arcane
+    } else if (movie.id == "81356") {
+      return setTrailerUrl("zmgYlYw7Uwk"); //arcane
+    } else {
+      // get the full url but only need the parameter after v=?
+      //https://www.youtube.com/watch?v=rUt5gnQjj04
+      movieTrailer(
+        movie?.name ||
+          movie?.title ||
+          movie?.original_name ||
+          movie.overview ||
+          ""
+      )
+        .then((url) => {
+          const temp = new URL(url).search;
+          const urlParams = new URLSearchParams(temp); // get the part after "?"
+
+          setTrailerUrl(urlParams.get("v")); //rUt5gnQjj04
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -29,6 +78,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -37,7 +87,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
-      {/* container -> post */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
